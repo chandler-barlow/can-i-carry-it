823da2b850e84cbc1ac861d574e4e6185509c685
chandler-barlow <chandlerbrlw@gmail.com>
2023-03-02 21:38:00 -0600

    made everything into components and added airline data mapping for the form, maybe it works, maybe it doesn't

---
+++ src/App/@Components/BagDimensionForm.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { Input } from "../../@Components/atoms/Input";
+
+type BagDimensionsFormProps = {
+  handleSubmit: (
+    height: number,
+    width: number,
+    length: number
+  ) => void;
+};
+
+export const BagDimensionsForm: React.FC<
+  BagDimensionsFormProps
+> = ({ handleSubmit }) => {
+  const [width, setWidth] = React.useState(0);
+  const [height, setHeight] = React.useState(0);
+  const [length, setLength] = React.useState(0);
+
+  function _handleSubmit(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void {
+    handleSubmit(height, width, length);
+  }
+
+  function validNumber(n: number): boolean {
+    return n > 0 && n < 100;
+  }
+
+  function handleHeight(
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void {
+    const sHeight: string = e.target.value;
+    const nHeight: number = parseInt(sHeight);
+    if (validNumber(nHeight)) {
+      setHeight(nHeight);
+    }
+  }
+
+  function handleWidth(
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void {
+    const sWidth: string = e.target.value;
+    const nWidth: number = parseInt(sWidth);
+    if (validNumber(nWidth)) {
+      setWidth(nWidth);
+    }
+  }
+
+  function handleLength(
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void {
+    const sLength: string = e.target.value;
+    const nLength: number = parseInt(sLength);
+    if (validNumber(nLength)) {
+      setLength(nLength);
+    }
+  }
+
+  return (
+    <div className="w-60 sm:basis-4/12 basis-6/12 shadow-lg rounded-lg m-10 p-5 bg-white -mt-20">
+      <h1
+        title="Form Title"
+        className="font-medium text-2xl text-center mb-3"
+      >
+        Bag Dimensions
+      </h1>
+
+      <Input
+        label="Height"
+        placeholder="20"
+        value=""
+        onChange={handleHeight}
+      />
+
+      <Input
+        label="Width"
+        placeholder="20"
+        value=""
+        onChange={handleWidth}
+      />
+
+      <Input
+        label="Length"
+        placeholder="20"
+        value=""
+        onChange={handleLength}
+      />
+
+      <button
+        className="btn w-full btn-primary mt-6"
+        onClick={_handleSubmit}
+      >
+        Check
+      </button>
+    </div>
+  );
+};
---
+++ src/App/index.tsx
@@ -0,0 +1,72 @@
+import "./App.css";
+import React from "react";
+import { NavBar } from "../@Components/organisms/NavBar";
+import { BagDimensionsForm } from "./@Components/BagDimensionForm";
+import { AirlineTable } from "./@Components/AirlineTable";
+import { Footer } from "../@Components/organisms/Footer";
+import { Airline, airlineData } from "../airlineData";
+/** 
+  TODO:
+  - make the form and the table responsive ( looks like shit on mobile rn )
+  - pull the form into a separate component
+  - pull the table into a separate component
+  - add a filter for specific airlines
+  - add google adds
+  - add the functionality for the whole thing to actually tell you what airlines work or not lmaoo
+*/
+
+type AirlineRow = {
+  airline: string;
+  personalItem: boolean;
+  carryOn: boolean;
+};
+
+function App() {
+  const [airlineRows, setAirlineRows] = React.useState<
+    Array<AirlineRow>
+  >([]);
+
+  function handleSubmit(
+    height: number,
+    width: number,
+    length: number
+  ): void {
+    const validateAirline = (airline: Airline) => {
+      const personalItem =
+        height <= airline.personalItemDimensions.height &&
+        width <= airline.personalItemDimensions.width &&
+        length <= airline.personalItemDimensions.length;
+      const carryOn =
+        height <= airline.carryOnDimensions.height &&
+        width <= airline.carryOnDimensions.width &&
+        length <= airline.carryOnDimensions.length;
+      return {
+        airline: airline.name,
+        personalItem,
+        carryOn,
+      };
+    };
+    const newAirlineRows = airlineData.map(validateAirline);
+    setAirlineRows(newAirlineRows);
+    console.log(newAirlineRows);
+    return;
+  }
+
+  return (
+    <div className="App h-screen bg-slate-200 flex flex-col justify-between">
+      <NavBar />
+
+      <div
+        title="content"
+        className="flex flex-col md:flex-row"
+      >
+        <BagDimensionsForm handleSubmit={handleSubmit} />
+        <AirlineTable airlineRows={airlineRows} />
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
